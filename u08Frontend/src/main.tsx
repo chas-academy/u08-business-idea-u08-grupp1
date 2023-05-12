d65314253d99f48c61b6f3d9f1a1c458dbbe1006
Freddo <fredrik.gullin@gmail.com>
2023-05-12 19:23:14 +0200

    tailwindcss

--- u08Frontend/src/main.tsx
+++ u08Frontend/src/main.tsx
@@ -1,12 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Test } from './tests'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <h1>u08 - grupp 1</h1>
+    )
+  },
+  {
+    path: 'test',
+    element: (
+      <Test></Test>
     )
   }
 ])
---
+++ u08Frontend/src/tests.tsx
@@ -0,0 +1,9 @@
+// ENDAST FÖR ATT TESTA ATT TAILWIND FUNGERAR
+
+export function Test() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
+  }

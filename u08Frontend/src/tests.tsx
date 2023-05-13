dfda48ab32828c9750f26d2cb8ed825ca91df768
VictorHedegran <106310946+VictorHedegran@users.noreply.github.com>
2023-05-13 17:17:20 +0200

    Delete tests.tsx

--- u08Frontend/src/tests.tsx
+++
@@ -1,9 +0,0 @@
-// ENDAST FÖR ATT TESTA ATT TAILWIND FUNGERAR
-
-export function Test() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }

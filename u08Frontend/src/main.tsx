27aa5ba94cdd3620032368ad7a57e7f683b07140
Arron-Reed <arron.reed@chasacademy.se>
2023-05-16 19:37:42 +0200

    First commit Navbar

--- u08Frontend/src/main.tsx
+++ u08Frontend/src/main.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
+      <>
+      <Navbar />
       <h1>u08 - grupp 1</h1>
+      </>
+
     )
   }
 ])

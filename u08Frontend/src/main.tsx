ed583cc68aed91af00496f746e8447e5bdd5d517
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-05-16 20:28:03 +0200

    footer design example

--- u08Frontend/src/main.tsx
+++ u08Frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
+      <>
       <h1>u08 - grupp 1</h1>
+      <Footer></Footer>
+      </>
     )
   }
 ])

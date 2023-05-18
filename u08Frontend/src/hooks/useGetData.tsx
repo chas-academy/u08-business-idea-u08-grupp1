5e9344ed06bea4d9bb5390b47b330728de19cce9
Freddo <fredrik.gullin@gmail.com>
2023-05-18 11:27:33 +0200

    useGetData2

--- u08Frontend/src/hooks/useGetData.tsx
+++ u08Frontend/src/hooks/useGetData.tsx
@@ -1,28 +1,42 @@
 // useGetData hook for converting JSON data (north / east - long / lat).
 import React, { useEffect, useState } from 'react';
-import MarkerF from './MarkerF'; 
 
-const apiURL = 'https://apigw.stockholm.se/NoAuth/VirtualhittaserviceDMZ/Rest/serviceunits?&filter%5Bservicetype.id%5D=122&page%5Blimit%5D=1500&page%5Boffset%5D=0&sort=name';
+// interface Gym {
+//     id: number;
+//     name: string;
+//     // Add other properties as needed
+// }
 
-//fetch gym locations
-async function fetchGymLocations() {
+const apiURL = 'https://apigw.stockholm.se/NoAuth/VirtualhittaserviceDMZ/Rest/serviceunits?&filter[servicetype.id]=122&page[limit]=1500&page[offset]=0&sort=name';
+
+export const UseGetData = () => {
+
+    const [gymData, setGymData] = useState([]);
+
+    useEffect(() => {
+        fetchGymLocations();
+    }, []);
+
+    //fetch gym locations
+    async function fetchGymLocations() {
     try {
         const response = await fetch(apiURL);
         const responseJson = await response.json();
-        const gymData = responseJson.data;
+        const fetchedGymData = responseJson.data;
 
-        //get names and positions
-        const gyms = gymData.map((gym) => { //man behöver deklarera gym
-            const gymName = gym.attributes.name;
-            const position = {
-                latitude: gym.attributes.location.north,
-                longitude: gym.attributes.location.east
-            };
-            return { gymName, position};
-        });
-        return gyms;
-    } catch (error) {
-        console.error('Error fetching gym locations: ', error);
-        return [];
+        setGymData(fetchedGymData);
+        console.log(fetchedGymData);
+        console.log(fetchedGymData[0].attributes.name);
+        console.log(fetchedGymData[0].attributes.location);
+        } catch (error) {
+            console.error('Error fetching gym locations: ', error);
+            return [];
+            }
     }
+
+    return (
+        <>
+            <h1>UseGetData</h1>
+        </>
+    )
 }

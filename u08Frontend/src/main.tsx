c72b1e87d371c573495244bf052a0031cab2f041
Freddo <fredrik.gullin@gmail.com>
2023-05-18 11:30:09 +0200

    patch

--- u08Frontend/src/main.tsx
+++ u08Frontend/src/main.tsx
@@ -1,12 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { UseGetData } from './hooks/useGetData'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <h1>u08 - grupp 1</h1>
+    )
+  },
+  {
+    path: '/usegetdata',
+    element: (
+      <UseGetData />
     )
   }
 ])

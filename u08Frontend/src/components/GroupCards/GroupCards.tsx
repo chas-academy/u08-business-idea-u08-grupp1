33b62505fdefed2d372252d6c422c57c7607bc45
Arron-Reed <arron.reed@chasacademy.se>
2023-05-30 14:51:57 +0200

    beta css styling

---
+++ u08Frontend/src/components/GroupCards/GroupCards.tsx
@@ -0,0 +1,150 @@
+import "./GroupCards.css";
+
+const GroupCards = () => {
+  return (
+    <div className="team">
+        <h2>Meet The Team</h2>
+    <div className="groupCards">
+      <div className="card">
+        <div className="card-image">
+          <img src="/src/assets/images/Arron-Reed.png" alt="Arron Reed" />
+        </div>
+        <p className="name">Arron Reed</p>
+        <p>FullStack Developer</p>
+        <p>
+          Not just a pretty face, Arron can fix any problem that comes his way
+          as well as cook a mean lasagne!
+        </p>
+        <div className="socials">
+          <button className="github">
+            <i className="fab fa-github"></i>
+          </button>
+          <button className="twitter">
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button className="pinterest">
+            <i className="fab fa-pinterest-p"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          <img src="/src/assets/images/Arron-Reed.png" alt="Arron Reed" />
+        </div>
+        <p className="name">Arron Reed</p>
+        <p>FullStack Developer</p>
+        <p>
+          Not just a pretty face, Arron can fix any problem that comes his way
+          as well as cook a mean lasagne!
+        </p>
+        <div className="socials">
+          <button className="github">
+            <i className="fab fa-github"></i>
+          </button>
+          <button className="twitter">
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button className="pinterest">
+            <i className="fab fa-pinterest-p"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          <img src="/src/assets/images/Arron-Reed.png" alt="Arron Reed" />
+        </div>
+        <p className="name">Arron Reed</p>
+        <p>FullStack Developer</p>
+        <p>
+          Not just a pretty face, Arron can fix any problem that comes his way
+          as well as cook a mean lasagne!
+        </p>
+        <div className="socials">
+          <button className="github">
+            <i className="fab fa-github"></i>
+          </button>
+          <button className="twitter">
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button className="pinterest">
+            <i className="fab fa-pinterest-p"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          <img src="/src/assets/images/Arron-Reed.png" alt="Arron Reed" />
+        </div>
+        <p className="name">Arron Reed</p>
+        <p>FullStack Developer</p>
+        <p>
+          Not just a pretty face, Arron can fix any problem that comes his way
+          as well as cook a mean lasagne!
+        </p>
+        <div className="socials">
+          <button className="github">
+            <i className="fab fa-github"></i>
+          </button>
+          <button className="twitter">
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button className="pinterest">
+            <i className="fab fa-pinterest-p"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          <img src="/src/assets/images/Arron-Reed.png" alt="Arron Reed" />
+        </div>
+        <p className="name">Arron Reed</p>
+        <p>FullStack Developer</p>
+        <p>
+          Not just a pretty face, Arron can fix any problem that comes his way
+          as well as cook a mean lasagne!
+        </p>
+        <div className="socials">
+          <button className="github">
+            <i className="fab fa-github"></i>
+          </button>
+          <button className="twitter">
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button className="pinterest">
+            <i className="fab fa-pinterest-p"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          <img src="/src/assets/images/Arron-Reed.png" alt="Arron Reed" />
+        </div>
+        <p className="name">Arron Reed</p>
+        <p>FullStack Developer</p>
+        <p>
+          Not just a pretty face, Arron can fix any problem that comes his way
+          as well as cook a mean lasagne!
+        </p>
+        <div className="socials">
+          <button className="github">
+            <i className="fab fa-github"></i>
+          </button>
+          <button className="twitter">
+            <i className="fab fa-twitter"></i>
+          </button>
+          <button className="pinterest">
+            <i className="fab fa-pinterest-p"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default GroupCards;
